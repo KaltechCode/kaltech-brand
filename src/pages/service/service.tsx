f34faf9848031a1af3ffd086e55654e3283b7ea1
kaltechCode <kaltechconsultancy@gmail.com>
2025-12-09 15:25:30 -0500

    debuging error

--- src/pages/service/service.tsx
+++ src/pages/service/service.tsx
@@ -43,7 +43,7 @@ const ServiceMain = () => {
     servicePanel();
   }, []);
   useLayoutEffect(() => {
-   servicePanelservicePanel
+    servicePanel();
   }, [hasRendered]);
 
   const handleOpen = () => {
